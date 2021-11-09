cd519f9fa0c541abd6c44423e171a826d06db028
Hu Andrew <ah458@bcps.org>
2021-11-09 12:53:45 -0500

    Initial commit

---
+++ src/core/Game.java
@@ -0,0 +1,65 @@
+// added a comment
+
+package core;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends BasicGameState 
+{	
+	int id;
+	
+	Game(int id) 
+	{
+		this.id = id;
+	}
+	
+	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException 
+	{
+		gc.setShowFPS(true);
+	}
+
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException 
+	{
+		
+
+	}
+
+	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException
+	{	
+		
+	}
+
+	public void enter(GameContainer gc, StateBasedGame sbg) throws SlickException 
+	{
+		
+	}
+
+	public void leave(GameContainer gc, StateBasedGame sbg) 
+	{
+		
+	}
+
+
+	public void keyPressed(int key, char c)
+	{
+		
+	}
+	
+	public void mousePressed(int button, int x, int y)
+	{
+		
+	}
+	
+	public int getID() 
+	{
+		return id;
+	}
+
+
+}
